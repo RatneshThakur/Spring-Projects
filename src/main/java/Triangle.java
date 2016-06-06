2ebb1795ee5bd0713bfdbd0be14a28e23a6c215d
RatneshThakur <ratnesh_thakur@ymail.com>
2016-06-06 00:12:20 -0700

    constructor injection

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,16 +1,27 @@
-
 public class Triangle {
-	public String type;
+	private String type;
+	private int height;
 	
 	public String getType() {
 		return type;
 	}
-
-	public void setType(String type) {
+	
+	/*public void setType(String type) {
 		this.type = type;
+	}*/
+	
+	public int getHeight() {
+		return height;
 	}
 
+	public Triangle(String type_var) {
+		type = type_var;
+	}
+	public Triangle(String type_var, int height_var) {
+		type = type_var;
+		height = height_var;
+	}
 	public void draw(){
-		System.out.println(getType()+" Triangle drawn");
+		System.out.println(getType()+" Triangle drawn of " + getHeight());
 	}
 }
